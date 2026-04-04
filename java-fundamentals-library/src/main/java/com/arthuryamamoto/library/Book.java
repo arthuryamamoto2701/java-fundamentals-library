82a6e27edfc393087cd17f6e21615d7ff1fc6e71
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-04-04 14:55:49 -0300

    feat: add Getters and Setters

--- java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Book.java
+++ java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Book.java
@@ -7,6 +7,39 @@ public class Book {
     private String author;
     private int year;
     private boolean available;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public boolean getAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    
 
     
     
