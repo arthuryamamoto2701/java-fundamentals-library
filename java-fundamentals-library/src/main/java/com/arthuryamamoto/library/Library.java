da7c2158686adfb291594230613a70e6a8cbc5f1
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-04-04 15:17:57 -0300

    feat: initialize Scanner in main method

--- java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Library.java
+++ java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Library.java
@@ -1,10 +1,13 @@
 package com.arthuryamamoto.library;
+
+import java.util.Scanner;
+
 public class Library {
     
 
 public static void main(String[] args) {
         
-    
+    Scanner scanner = new Scanner(System.in);
 
     }
 }
