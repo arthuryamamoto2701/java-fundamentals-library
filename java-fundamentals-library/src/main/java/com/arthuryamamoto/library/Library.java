279d118e2df0a66bc42198ae6ba478057cebc83c
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-04-04 15:21:59 -0300

    feat: create ArrayList to manage books

--- java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Library.java
+++ java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Library.java
@@ -1,5 +1,6 @@
 package com.arthuryamamoto.library;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
@@ -8,6 +9,11 @@ public class Library {
 public static void main(String[] args) {
         
     Scanner scanner = new Scanner(System.in);
+
+    ArrayList<Book> books = new ArrayList<>();
+    
+
+
 
     }
 }
