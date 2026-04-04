9e06114287194b35d3596d80adebe078bf1fec8b
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-04-04 14:43:13 -0300

    feat: add Book class

---
+++ java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Book.java
@@ -0,0 +1,13 @@
+package com.arthuryamamoto.library;
+
+public class Book {
+
+
+    private int id;
+    private String title;
+    private String autor;
+    private int ano;
+
+    
+    
+}
--- java-fundamentals-library/src/main/java/com/arthuryamamoto/library/Library.java
+++
@@ -1,5 +0,0 @@
-package com.arthuryamamoto.library;
-
-public class Library {
-    
-}
